5d32dec4ebab3013a050986d9f97e096cb95ab08
DanielPrawin <44159183+DanielPrawin@users.noreply.github.com>
2023-08-12 00:36:59 +0530

    7

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -1,16 +1,16 @@
 def newGit(newRepo)
 {
-  echo "git https://github.com/DanielPrawin/${newRepo}.git"
+  "git https://github.com/DanielPrawin/${newRepo}.git"
 }
 def build()
 {
-  echo "sh mvn package"
+  "sh mvn package"
 }
 def deploy(jobname, ip,contextpath)
 {
-  echo "sh scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${contextpath}.war"
+  "sh scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${contextpath}.war"
 }
 def testing(jobname)
 {
-  echo "sh java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
+  "sh java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
 }
