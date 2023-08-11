2763015b2cedc9f91ede103a511df7ae6057c976
DanielPrawin <44159183+DanielPrawin@users.noreply.github.com>
2023-08-12 00:27:20 +0530

    Update cicd.groovy1

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -1,16 +1,16 @@
-dev newGit(newRepo)
+def newGit(newRepo)
 {
   echo "git https://github.com/DanielPrawin/${newRepo}.git"
 }
-dev build()
+def build()
 {
   echo "sh mvn package"
 }
-dev deploy(jobname, ip,contextpath)
+def deploy(jobname, ip,contextpath)
 {
   echo "sh scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${contextpath}.war"
 }
-dev testing(jobname)
+def testing(jobname)
 {
   echo "sh java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
 }
